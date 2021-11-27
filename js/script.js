59da3dac39c904e7f0a7738a22a07b3128693482
ミカド <akbarandy99@gmail.com>
2021-11-28 00:36:41 +0800

    fill data

--- js/script.js
+++ js/script.js
@@ -6,14 +6,14 @@ card.forEach(function(item){
 	$('#card-list').append(
 		`<div class="col-md-6 col-lg-4 glry-item gallery-masonry-item col-sm-12 ${item.tag}">
 		    <div class="portfolio-item-img">
-		        <img src="img/portfolio/${item.gambar}" alt="Portfolio Img" class="img-fluid portfolio-img">
-		        <h5 class="portfolio-detail-title">Web Design</h5>
+		        <img style="background: #E7F2F8;" src="img/portfolio/${item.gambar}" alt="Portfolio Img" class="img-fluid portfolio-img">
+		        <h5 style="text-align: -webkit-center;" class="portfolio-detail-title">${item.deskripsi}</h5>
 		    </div>
 		    <div class="portfolio-buttons">
-		        <a href="img/portfolio/portfolio_masonry_01.png" class="portfolio-zoom-link">
+		        <a href="img/portfolio/${item.gambar}" class="portfolio-zoom-link">
 		            <i class="fas fa-search"></i>
 		        </a>
-		        <a href="#0" class="portfolio-detail-link">
+		        <a href="${item.link}" class="portfolio-detail-link">
 		            <i class="fas fa-link"></i>
 		        </a>
 		    </div>
