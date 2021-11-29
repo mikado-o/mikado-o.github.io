7e32fd59f3ab6383d99a707f7ffcdcf0c965d6f4
ミカド <akbarandy99@gmail.com>
2021-11-29 13:58:04 +0800

    optimizing

--- js/script.js
+++ js/script.js
@@ -20,18 +20,3 @@ card.forEach(function(item){
 		</div>`
 	)
 })
-
-// <div class="col-md-6 col-lg-4 glry-item gallery-masonry-item col-sm-12 web">
-//     <div class="portfolio-item-img">
-//         <img src="img/portfolio/portfolio_masonry_01.png" alt="Portfolio Img" class="img-fluid portfolio-img">
-//         <h5 class="portfolio-detail-title">Web Design</h5>
-//     </div>
-//     <div class="portfolio-buttons">
-//         <a href="img/portfolio/portfolio_masonry_01.png" class="portfolio-zoom-link">
-//             <i class="fas fa-search"></i>
-//         </a>
-//         <a href="#0" class="portfolio-detail-link">
-//             <i class="fas fa-link"></i>
-//         </a>
-//     </div>
-// </div>
